84a67f83dde8a18f134fe498f8f66e9324b02dba
david.yuan <2248568234@qq.com>
2019-04-26 11:05:23 +0800

    .

---
+++ js/001.js
@@ -0,0 +1,8 @@
+function toChange(){
+			var obox=document.getElementById('box');
+			obox.style.width='500px';
+			obox.style.height='500px';
+			obox.style.background = 'red';
+		}
+		var obox=document.getElementById('box');
+		obox.onclick='toChange';
